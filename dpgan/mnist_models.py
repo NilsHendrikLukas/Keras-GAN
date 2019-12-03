065178ef26a07398d3628bcc81e1165b1f4b9295
Lucas Coelho <lcoelho@uwaterloo.ca>
2019-12-03 04:11:16 -0500

    new implementation of dpgan

---
+++ dpgan/mnist_models.py
@@ -0,0 +1,63 @@
+from keras import Sequential, Input, Model
+from keras.layers import Dense, LeakyReLU, BatchNormalization, Reshape, Flatten, Conv2D, Dropout, \
+    ZeroPadding2D, UpSampling2D, Activation
+
+
+def build_critic():
+    img_shape = (28, 28, 3)
+
+    model = Sequential()
+
+    model.add(Conv2D(16, kernel_size=3, strides=2, input_shape=img_shape, padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(32, kernel_size=3, strides=2, padding="same"))
+    model.add(ZeroPadding2D(padding=((0, 1), (0, 1))))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(128, kernel_size=3, strides=1, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(1))
+
+    model.summary()
+
+    img = Input(shape=img_shape)
+    validity = model(img)
+
+    return Model(img, validity)
+
+
+def build_generator():
+    latent_dim = 100
+    channels = 1
+
+    model = Sequential()
+
+    model.add(Dense(128 * 7 * 7, activation="relu", input_dim=latent_dim))
+    model.add(Reshape((7, 7, 128)))
+    model.add(UpSampling2D())
+    model.add(Conv2D(128, kernel_size=4, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
+    model.add(UpSampling2D())
+    model.add(Conv2D(64, kernel_size=4, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
+    model.add(Conv2D(channels, kernel_size=4, padding="same"))
+    model.add(Activation("tanh"))
+
+    model.summary()
+
+    noise = Input(shape=(latent_dim,))
+    img = model(noise)
+
+    return Model(noise, img)
+
