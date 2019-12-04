100868f5d62df2afb7a7b80b2ff1c812235d0837
Lucas Coelho <lcoelho@uwaterloo.ca>
2019-12-03 20:45:22 -0500

    Smaller attacker and more epochs

--- dpgan/ppgan.py
+++ dpgan/ppgan.py
@@ -147,9 +147,9 @@ class PPGAN():
         """
         advreg_in = Input(input_shape)
 
-        l0 = Dense(units=512)(advreg_in)
+        l0 = Dense(units=128)(advreg_in)
         l1 = Dropout(0.2)(l0)
-        l2 = Dense(units=256)(l1)
+        l2 = Dense(units=64)(l1)
         l3 = Dropout(0.2)(l2)
         l4 = Dense(units=10)(l3)
 
@@ -266,7 +266,7 @@ class PPGAN():
         Takes the classifiers featuremaps and predicts on them
         """
         test_size = 256
-        epochs = 5
+        epochs = 10
         batch_size = min(128, len(self.X_train))
 
         for e in range(epochs):
