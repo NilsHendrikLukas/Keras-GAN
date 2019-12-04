3311c79edc2c3f5eea2f6f7849d7d5d66f0856d7
Lucas Coelho <lcoelho@uwaterloo.ca>
2019-12-04 08:24:10 -0500

    Properly inverted it this time

--- dpgan/ppgan.py
+++ dpgan/ppgan.py
@@ -287,8 +287,8 @@ class PPGAN():
         y_preds_out = self.advreg_model.predict(self.x_out[idx_out])
 
         # -1 means in, 1 means out
-        print("Accuracy In: {}".format(len(np.where(np.sign(y_preds_in) == -1)[0])))
-        print("Accuracy Out: {}".format(len(np.where(np.sign(y_preds_out) == 1)[0])))
+        print("Accuracy In: {}".format(len(np.where(np.sign(y_preds_in) == 1)[0])))
+        print("Accuracy Out: {}".format(len(np.where(np.sign(y_preds_out) == -1)[0])))
 
         """
             True negatives
