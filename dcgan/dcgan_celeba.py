835df670fd37b130d8c63cc90a01e137e09e35f4
nlukas <nlukas@uwaterloo.ca>
2020-01-13 10:50:16 -0500

    dcgan celeba

--- dcgan/dcgan_celeba.py
+++ dcgan/dcgan_celeba.py
@@ -163,7 +163,7 @@ class DCGAN():
         cnt = 0
         for i in range(r):
             for j in range(c):
-                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0])
                 axs[i,j].axis('off')
                 cnt += 1
         fig.savefig("Keras-GAN/dcgan/images/celeba_%d.png" % epoch)
